3b6b30bb666ac6776f35e6ba5091275232b175cf
mrprotocoll <lekanvgbg@gmail.com>
2023-08-13 22:39:22 +0100

    Add socialite for oauth

---
+++ app/Http/Controllers/Api/V1/Auth/GoogleAuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        //
+        $request->validate([
+            'token' => ['required', 'string', 'max:255'],
+        ]);
+
+        $googleUser = Socialite::driver('github')->userFromToken($request->token);
+        $device = substr($request->userAgent() ?? '', 0, 255);
+
+        $user = User::updateOrCreate([
+            'google_id' => $googleUser->id,
+        ], [
+            'name' => $googleUser->name,
+            'email' => $googleUser->email,
+            'password' => $device,
+            'google_token' => $googleUser->token,
+            'google_refresh_token' => $googleUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        $token = $user->createToken($device)->plainTextToken;
+
+        return response()->json(['token' => $token, 'data' => new UserResource($user)], 201);
+    }
+}
--- config/auth.php
+++ config/auth.php
@@ -112,4 +112,10 @@ return [
 
     'password_timeout' => 10800,
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
+
 ];
