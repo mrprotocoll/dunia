aaa84283de75537ae5d68fe8b7bed784e4838d30
mrprotocoll <lekanvgbg@gmail.com>
2023-08-20 01:32:35 +0100

    Add order

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -23,17 +23,23 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(OrderRequest $request)
+    public function store(Request $request)
     {
         $user = Auth::user();
         DB::transaction(function () use ($user, $request) {
-            $order = $user->orders()->create([]);
-            foreach($request->products as $productData){
-                $product = Product::findOrFail($productData['id']);
-                $quantity = $productData['quantity'];
-                $total_price = intval($productData['quantity']) * floatval($product->price);
+            $shipping = $request['shipping'];
+            $order = $user->orders()->create([
+                'total_price' => $request['total'],
+                'shipping_price' => $shipping['price'] ?? 0,
+                'billing_address_id' => $shipping['destination']
+            ]);
 
-                $order->products()->attach($product, ['quantity' => $quantity, 'total' => $total_price]);
+            foreach($request['cart'] as $productData){
+                $product = Product::findOrFail($productData['product_id']);
+                $quantity = intval($productData['quantity']);
+                $total_price = $quantity * floatval($product->price);
+
+                $order->products()->attach($product, ['quantity' => $quantity, 'total_price' => $total_price]);
             }
         });
     }
@@ -51,26 +57,67 @@ class OrderController extends Controller
             'success_url' => ['required', 'url'],
             'cancel_url' => ['required', 'url'],
             'cart' => ['required', 'array'],
-            'shipping' => []
+            'cart.*.product_id' => ['required', 'exists:products,id'],
+            'cart.*.quantity' => ['required', 'numeric', 'gt:0'],
+            'shipping' => ['array'],
+            'shipping.price' => ['required_if:shipping,*'],
+            'shipping.destination' => ['required_if:shipping,*','exists:billing_addresses,id']
         ]);
 
         $stripe = new StripeClient(env('STRIPE_SECRET'));
 
-        $checkout_session = $stripe->checkout->sessions->create([
-            'line_items' => [[
+        $line_items = [];
+        $shipping = $request['shipping'];
+        $request['total'] = 0;
+        foreach ($request['cart'] as $item) {
+            $product = Product::find($item['product_id']);
+            // Add print_price to product price if shipping is involved
+            $price = $shipping
+                ? floatval($product->price) + floatval($product->print_price)
+                : floatval($product->price);
+
+            // reset total price of all ordered products in cart
+            $request['total'] += $price * intval($item['quantity']);
+
+            // add product data to stripe items
+            $line_items[] = [
                 'price_data' => [
                     'currency' => 'usd',
                     'product_data' => [
-                        'name' => 'T-shirt',
+                        'name' => $product->name,
                     ],
-                    'unit_amount' => 2000,
+                    'unit_amount' => $price * 100,
+                ],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
+        if($request['shipping']) {
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'usd',
+                    'product_data' => [
+                        'name' => 'Shipping',
+                    ],
+                    'unit_amount' => $shipping['price'] * 100,
                 ],
                 'quantity' => 1,
-            ]],
+            ];
+        }
+
+        $checkout_session = $stripe->checkout->sessions->create([
+            'line_items' => $line_items,
             'mode' => 'payment',
             'success_url' => $request->success_url,
             'cancel_url' => $request->cancel_url,
         ]);
+
+        if($checkout_session->url) {
+            // add a new order
+            $this->store($request);
+        }
+
+        return response()->json(['url' => $checkout_session->url]);
     }
 
 }
